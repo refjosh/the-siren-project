33c0e73e1f4e68cbd7e5bccbaa57e1c3f8a82094
joshtru <joshuaojc@gmail.com>
2019-10-18 22:09:31 -0500

    Separated landing thumb component from landing section

--- src/components/landing-section/landing-section.component.jsx
+++ src/components/landing-section/landing-section.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Carousel } from "antd";
+import LandingThumb from "../landing-thumb/landing-thumb.component";
 
 import "antd/dist/antd.css";
 import "./landing-section.styles.scss";
@@ -17,40 +18,24 @@ const LandingSection = () => {
           pauseOnFocus
           draggable={true}
         >
-          <div className="slider">
-            <img
-              className="slider__image"
-              src="https://www.newsbtc.com/wp-content/uploads/2019/10/shutterstock_683030524-1200x780.jpg"
-              alt="display"
-            />
-            <div className="slider__detail">
-              <span>/ 01</span>
-              <h3 className="slider__detail--title">
-                Arizona Ultimate Adventure – Grand Canyon & Beyond
-              </h3>
-              <p>
-                <span className="slider__detail--category">TRAVEL</span>/
-                <span className="slider__detail--date">OCTOBER 18 2019 </span>
-              </p>
-            </div>
-          </div>
-          <div className="slider">
-            <img
-              className="slider__image"
-              src="https://cdn.arstechnica.net/wp-content/uploads/2019/10/connected-car-wifi-button-760x380.jpg"
-              alt="display"
-            />
-            <div className="slider__detail">
-              <span>/ 01</span>
-              <h3 className="slider__detail--title">
-                Arizona Ultimate Adventure – Grand Canyon & Beyond
-              </h3>
-              <p>
-                <span className="slider__detail--category">TRAVEL</span>/
-                <span className="slider__detail--date">OCTOBER 18 2019 </span>
-              </p>
-            </div>
-          </div>
+          <LandingThumb
+            index={"01"}
+            title={"Arizona Ultimate Adventure – Grand Canyon & Beyond"}
+            imageUrl={
+              "https://www.newsbtc.com/wp-content/uploads/2019/10/shutterstock_683030524-1200x780.jpg"
+            }
+            category={"General"}
+            date={"October 18 2019"}
+          />
+          <LandingThumb
+            index={"02"}
+            title={"Arizona Ultimate Adventure – Grand Canyon & Beyond"}
+            imageUrl={
+              "https://www.newsbtc.com/wp-content/uploads/2019/10/shutterstock_683030524-1200x780.jpg"
+            }
+            category={"General"}
+            date={"October 18 2019"}
+          />
         </Carousel>
       </div>
       <div className="landing-section__seconday-section"></div>
