f32e6ff41de6793a23137f74531cf527aa306f0f
joshtru <joshuaojc@gmail.com>
2019-10-18 20:59:38 -0500

    Added slider for the landing section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Header from "./components/header/header.component";
+import HomePage from "./pages/homepage/homepage.component";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <HomePage />
     </div>
   );
 }
