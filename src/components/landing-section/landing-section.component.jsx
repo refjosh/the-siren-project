c68508dcd42f0fc25dd983060d52675c22c5e847
joshtru <joshuaojc@gmail.com>
2019-10-21 14:44:06 -0500

    Added row and col for responsiveness

--- src/components/landing-section/landing-section.component.jsx
+++ src/components/landing-section/landing-section.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { compose } from "redux";
 
-import { Carousel } from "antd";
+import { Carousel, Row, Col } from "antd";
 import LandingSlider from "../landing-slider/landing-slider.component";
 import LandingThumb from "../landing-thumb/landing-thumb.component";
 
@@ -16,43 +16,49 @@ import "./landing-section.styles.scss";
 const LandingSection = ({ topHeadlines }) => {
   return (
     <div className="landing-section">
-      <div className="landing-section__main-slider">
-        <Carousel
-          speed={2500}
-          autoplay
-          autoplaySpeed={5000}
-          dotPosition={"right"}
-          dots={true}
-          pauseOnFocus
-          draggable={true}
-        >
+      <Row gutter={[8, 16]}>
+        <Col xs={24} sm={24} md={24} lg={16} xl={16}>
+          <div className="landing-section__main-slider">
+            <Carousel
+              speed={2500}
+              autoplay
+              autoplaySpeed={5000}
+              dotPosition={"right"}
+              dots={true}
+              pauseOnFocus
+              draggable={true}
+            >
+              {topHeadlines
+                .filter((item, idx) => idx < 4)
+                .map((item, index) => (
+                  <LandingSlider
+                    key={index + 1}
+                    index={index + 1}
+                    title={item.title}
+                    imageUrl={item.urlToImage}
+                    category={item.source.name}
+                    date={item.publishedAt}
+                  />
+                ))}
+            </Carousel>
+          </div>
+        </Col>
+        <Col xs={24} sm={24} md={24} lg={8} xl={8}>
+          {/* <div className="landing-section__seconday-section"> */}
           {topHeadlines
-            .filter((item, idx) => idx < 4)
-            .map((item, index) => (
-              <LandingSlider
-                key={index + 1}
-                index={index + 1}
-                title={item.title}
-                imageUrl={item.urlToImage}
-                category={item.source.name}
-                date={item.publishedAt}
+            .filter((item, idx) => idx >= 4)
+            .map((headline, index) => (
+              <LandingThumb
+                key={index}
+                title={headline.title}
+                imageUrl={headline.urlToImage}
+                category={headline.source.name}
+                date={extractDate(headline.publishedAt)}
               />
             ))}
-        </Carousel>
-      </div>
-      <div className="landing-section__seconday-section">
-        {topHeadlines
-          .filter((item, idx) => idx >= 4)
-          .map((headline, index) => (
-            <LandingThumb
-              key={index}
-              title={headline.title}
-              imageUrl={headline.urlToImage}
-              category={headline.source.name}
-              date={extractDate(headline.publishedAt)}
-            />
-          ))}
-      </div>
+          {/* </div> */}
+        </Col>
+      </Row>
     </div>
   );
 };
