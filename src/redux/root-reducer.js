2888fd43a45560478815dfd5bfde3e848a592f38
joshtru <joshuaojc@gmail.com>
2019-10-20 20:09:28 -0500

    Now persisting state with redux persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,19 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage/session";
+
 import categoryReducer from "./category/category.reducer";
 import headlineReducer from "./headline/headline.reducer";
+
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  whitelist: ["headline", "category"]
+};
 
 const rootReducer = combineReducers({
   category: categoryReducer,
   headline: headlineReducer
 });
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
 import createSaga from "redux-saga";
 
 import rootReducer from "./root-reducer";
@@ -14,7 +15,8 @@ if (process.env.NODE_ENV === "development") {
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const persistor = persistStore(store);
 
 sagaMiddle.run(rootSagas);
 
-export default store;
+export default { store, persistor };
